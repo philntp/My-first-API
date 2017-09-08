b47c07cfc5db4acaddf9c5c1b7a0888a4fa9e3b4
nuttaphat santiphan <nuttaphat.santiphan@g.swu.ac.th>
2017-09-08 22:34:56 +0700

    complete

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,76 @@
-"use strict";
+'use strict';
+
+var getBeersAPI = function getBeersAPI() {
+    return fetch('https://api.punkapi.com/v2/beers').then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        var beers = response;
+        var result = "";
+        for (var i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+        }
+        document.getElementById('result').innerHTML = result;
+    });
+};
+
+var getBeersByName = function getBeersByName(name) {
+    return fetch('https://api.punkapi.com/v2/beers/?beer_name=' + name).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        var beers = response;
+        var result = "";
+        for (var i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+        }
+        document.getElementById('result').innerHTML = result;
+    });
+};
+
+var getBeersByYeast = function getBeersByYeast(yeast) {
+    return fetch('https://api.punkapi.com/v2/beers/?yeast=' + yeast).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        var beers = response;
+        var result = "";
+        for (var i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+        }
+        document.getElementById('result').innerHTML = result;
+    });
+};
+
+var getBeersByNameAndYeast = function getBeersByNameAndYeast(name, yeast) {
+    return fetch('https://api.punkapi.com/v2/beers/?beer_name=' + name + '&?yeast=' + yeast).then(function (response) {
+        return response.json();
+    }).catch(function (e) {
+        return console.err('Fail');
+    }).then(function (response) {
+        var beers = response;
+        var result = "";
+        for (var i = 0; i < beers.length; i++) {
+            result += "<b>Name : </b>" + beers[i].name + "<br> <b>Tagline : </b>" + beers[i].tagline + "<br> <b>PH : </b>" + beers[i].ph + "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast + "<br> <b>First Brewed : </b>" + beers[i].first_brewed + "<br> <b>Description : </b>" + beers[i].description + "<br>" + "<image src =" + beers[i].image_url + "/><hr>";
+        }
+        document.getElementById('result').innerHTML = result;
+    });
+};
+
+getBeersAPI();
+
+var search = function search(name, yeast) {
+    if (yeast != "" && name != "") {
+        getBeersByNameAndYeast(name, yeast);
+    } else if (yeast != "") {
+        getBeersByYeast(yeast);
+    } else if (name != "") {
+        getBeersByName(name);
+    } else {
+        var result = "No result";
+        document.getElementById('result').innerHTML = result;
+    }
+};
--- js/index.js
+++ js/index.js
@@ -1,8 +1,86 @@
-const getBeerDoc = () =>
-fetch('https://api.punkapi.com/v2/beers')
-
-getBeerDoc()
+const getBeersAPI = () => fetch('https://api.punkapi.com/v2/beers')
 .then(response => response.json())
-.catch(e => console.error('Something went wrong', e))
-.then(data => console.log('Hello', data.name))
+.catch(e => console.err('Fail'))
+.then(response => {
+    let beers = response;
+    let result = "";
+    for (let i = 0; i < beers.length; i++) {
+        result += "<b>Name : </b>" + beers[i].name +
+        "<br> <b>Tagline : </b>" + beers[i].tagline +
+        "<br> <b>PH : </b>" + beers[i].ph +
+        "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+        "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+        "<br> <b>Description : </b>" + beers[i].description +
+        "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+    }
+    document.getElementById('result').innerHTML = result
+})
 
+const getBeersByName = (name) => fetch(`https://api.punkapi.com/v2/beers/?beer_name=${name}`)
+.then(response => response.json())
+.catch(e => console.err('Fail'))
+.then(response => {
+    let beers = response;
+    let result = "";
+    for (let i = 0; i < beers.length; i++) {
+        result += "<b>Name : </b>" + beers[i].name +
+        "<br> <b>Tagline : </b>" + beers[i].tagline +
+        "<br> <b>PH : </b>" + beers[i].ph +
+        "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+        "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+        "<br> <b>Description : </b>" + beers[i].description +
+        "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+    }
+    document.getElementById('result').innerHTML = result
+})
+
+const getBeersByYeast = (yeast) => fetch(`https://api.punkapi.com/v2/beers/?yeast=${yeast}`)
+.then(response => response.json())
+.catch(e => console.err('Fail'))
+.then(response => {
+    let beers = response;
+    let result = "";
+    for (let i = 0; i < beers.length; i++) {
+        result += "<b>Name : </b>" + beers[i].name +
+        "<br> <b>Tagline : </b>" + beers[i].tagline +
+        "<br> <b>PH : </b>" + beers[i].ph +
+        "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+        "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+        "<br> <b>Description : </b>" + beers[i].description +
+        "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+    }
+    document.getElementById('result').innerHTML = result
+})
+
+const getBeersByNameAndYeast = (name,yeast) => fetch(`https://api.punkapi.com/v2/beers/?beer_name=${name}&?yeast=${yeast}`)
+.then(response => response.json())
+.catch(e => console.err('Fail'))
+.then(response => {
+    let beers = response;
+    let result = "";
+    for (let i = 0; i < beers.length; i++) {
+        result += "<b>Name : </b>" + beers[i].name +
+        "<br> <b>Tagline : </b>" + beers[i].tagline +
+        "<br> <b>PH : </b>" + beers[i].ph +
+        "<br> <b>Yeast : </b>" + beers[i].ingredients.yeast +
+        "<br> <b>First Brewed : </b>" + beers[i].first_brewed +
+        "<br> <b>Description : </b>" + beers[i].description +
+        "<br>" + "<image src =" + beers[i].image_url + "/><hr>"
+    }
+    document.getElementById('result').innerHTML = result
+})
+
+getBeersAPI()
+
+const search = (name, yeast) => {
+    if (yeast != "" && name != "") {
+        getBeersByNameAndYeast(name,yeast)
+    } else if (yeast != "") {
+        getBeersByYeast(yeast)
+    } else if (name != "") {
+        getBeersByName(name)
+    } else {
+        let result = "No result";
+        document.getElementById('result').innerHTML = result
+    }
+}
